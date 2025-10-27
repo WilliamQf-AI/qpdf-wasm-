e4103d874fa74c21489c77f4cf4c4f481352e945
夫子 <40328063+WilliamQf-AI@users.noreply.github.com>
2025-10-27 12:25:59 +0800

    Refactor FS object property assignment syntax

--- js/post.js
+++ js/post.js
@@ -1,14 +1,15 @@
+FS = typeof FS !== "undefined" ? FS : Module.FS;
 Object.assign(FS, {
-  ["init"]: FS.init,
-  ["mkdir"]: FS.mkdir,
-  ["mount"]: FS.mount,
-  ["unlink"]: FS.unlink,
-  ["unmount"]: FS.unmount,
-  ["chdir"]: FS.chdir,
-  ["writeFile"]: FS.writeFile,
-  ["readFile"]: FS.readFile,
-  ["createLazyFile"]: FS.createLazyFile,
-  ["setIgnorePermissions"]: function (val) {
+  init: FS.init,
+  mkdir: FS.mkdir,
+  mount: FS.mount,
+  unlink: FS.unlink,
+  unmount: FS.unmount,
+  chdir: FS.chdir,
+  writeFile: FS.writeFile,
+  readFile: FS.readFile,
+  createLazyFile: FS.createLazyFile,
+  setIgnorePermissions: function (val) {
     FS.ignorePermissions = val;
   },
 });
